6e2a7936f1da74de233e8a2c7b3ab0cd29b5cd9d
ABDERRAHIM JANBOUBI <81822929+JANBOUBI-ABDERRAHIM@users.noreply.github.com>
2023-01-07 01:21:59 +0100

    Update application_updated.py

--- application_updated.py
+++ application_updated.py
@@ -10,6 +10,7 @@ import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer("french")
